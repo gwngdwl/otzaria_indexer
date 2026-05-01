428bbe22c9d73ae37b8196cc3b1037f744a727fa
gaon gadol <gwngdwl@gmail.com>
2026-05-01 09:52:54 +0300

    Initialize project structure with essential files and configurations

---
+++ bin/otzaria_indexer.dart
@@ -0,0 +1,5 @@
+import 'package:otzaria_indexer/otzaria_indexer.dart' as otzaria_indexer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${otzaria_indexer.calculate()}!');
+}
---
+++ test/otzaria_indexer_test.dart
@@ -0,0 +1,8 @@
+import 'package:otzaria_indexer/otzaria_indexer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
